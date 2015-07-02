dcc619b126239aad805654c247d7f99a4d6d7e85
Jordan blank <jordan4ibanez@gmail.com>
2015-07-02 00:01:39 -0400

    Make the tile selection consistant no matter where the player is in their walking cycle

--- gui.lua
+++ gui.lua
@@ -4,8 +4,8 @@ end
 
 --allow for mouse environment interactions
 function drawselection()
-	selectionx = ((math.floor(((mousex)+windowcenter[1])/tilesize)*tilesize)-(windowcenter[1])) 
-	selectiony = (((math.floor((mousey-windowcenter[2])/tilesize + 0.5) * tilesize ) + windowcenter[2]) - (tilesize/2))
+	selectionx = ((math.floor(((mousex-movementfloat[1])+windowcenter[1])/tilesize)*tilesize)-(windowcenter[1]))+(movementfloat[1])
+	selectiony = (((math.floor((mousey-windowcenter[2]-movementfloat[2])/tilesize + 0.5) * tilesize ) + windowcenter[2]) - (tilesize/2))+movementfloat[2]
 
 	love.graphics.rectangle("line", selectionx, selectiony, tilesize , tilesize )
 end
